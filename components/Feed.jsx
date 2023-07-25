abf3ec32bd563afb4e58e0761fd6142b08f80113
Dias Mukash <diasmukash01@gmail.com>
2023-07-25 21:29:00 +0600

    added feed page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,50 @@
+'use client'
+
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+import PromptCard from './PromptCard';
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data}
+    </div>
+  )
+}
 
 function Feed() {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+     
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+    }
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList
+        data={[]}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
 
